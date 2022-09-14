61f8c8d0e2fc86f5811e47491d6e5a67642f8597
AylinGongora <106359127+AylinGongora@users.noreply.github.com>
2022-09-13 21:29:10 -0500

    Update lambda_function.py
    Actualizacion datos backend cvc

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,48 +2,76 @@ import json
 import boto3
 import re
 
-def lambda_handler(event, context)
+def lambda_handler(event, context):
     # TODO implement
-    # Obtengo el cliente de la IA de AWS
-    client = boto3.client(rekognition)
-    # Obtengo cliente de S3
-    s3 = boto3.client(s3)
-    fileObj = s3.get_object(Bucket = imgdetector, Key = imgstar01.jpeg)
-    file_content = fileObj[Body].read()
-    # response = client.detect_text(Image = {S3Object{Bucketimgdetector,Nameimgstar01.jpeg}})
-    response = client.detect_text(Image = {S3Object{Bucketimgdetector204646-dev,Namepublicmy-photo.jpg}})
+    client = boto3.client("rekognition")
+    s3 = boto3.client("s3")
+    fileObj = s3.get_object(Bucket = "imgdetector", Key = "imgs/tar01.jpeg")
+    file_content = fileObj["Body"].read()
+    # response = client.detect_text(Image = {"S3Object":{"Bucket":"imgdetector","Name":"imgs/tar01.jpeg"}})
+    response = client.detect_text(Image = {"S3Object":{"Bucket":"imgdetector204646-dev","Name":"public/my-photo.jpg"}})
     
     textDetections=response['TextDetections']
     nTarjeta = ''
     nVal = ''
     fExpira = ''
     fVal = ''
-    print ('Detected textn----------')
-    for text in textDetections
+    stringsValues = []
+    print ('Detected text\n----------')
+    for text in textDetections:
             dt = text['DetectedText']
-            val = {.2f}.format(text['Confidence']) + %
-            print ('Detected text' + text['DetectedText'])
-            print ('Confidence ' + {.2f}.format(text['Confidence']) + %)
-            print ('Id {}'.format(text['Id']))
-            if 'ParentId' in text
-                print ('Parent Id {}'.format(text['ParentId']))
-            print ('Type' + text['Type'])
+            val = "{:.2f}".format(text['Confidence']) + "%"
+            print ('Detected text:' + text['DetectedText'])
+            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+            print ('Id: {}'.format(text['Id']))
+            if 'ParentId' in text:
+                print ('Parent Id: {}'.format(text['ParentId']))
+            print ('Type:' + text['Type'])
             print()
             
-            if re.search(([0-9]{4})+[' ']+([0-9]{4})+[' ']+([0-9]{4})+[' ']+([0-9]{4}), dt)
-                print ('Numero tarjeta {}'.format(dt))
+            if re.search("([0-9]{4})+[' ']+([0-9]{4})+[' ']+([0-9]{4})+[' ']+([0-9]{4})", dt):
+                print ('Numero tarjeta: {}'.format(dt))
                 nTarjeta = dt
                 nVal = val
-            if re.search(([0-9]{2})+['']+([0-9]{2}), dt)
-                print ('Fecha Expira {}'.format(dt))
+            elif re.search("([0-9]{2})+['/']+([0-9]{2})", dt):
+                print ('Fecha Expira: {}'.format(dt))
                 fExpira = dt
                 fVal = val
+            else:
+                nombres = {
+                          "nombre": dt,
+                          "porcentaje": val
+                        }
+                stringsValues.append(nombres)
+                
+    response = client.detect_text(Image = {"S3Object":{"Bucket":"imgdetector204646-dev","Name":"public/cvc.jpg"}})
+    
+    textDetections=response['TextDetections']
+    nCvc = ''
+    pCvc = ''
+    print ('Detected text\n----------')
+    for text in textDetections:
+            dt = text['DetectedText']
+            val = "{:.2f}".format(text['Confidence']) + "%"
+            print ('Detected text:' + text['DetectedText'])
+            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+            print ('Id: {}'.format(text['Id']))
+            if 'ParentId' in text:
+                print ('Parent Id: {}'.format(text['ParentId']))
+            print ('Type:' + text['Type'])
+            print()
             
+            if re.search("[0-9]{3}", dt):
+                if len(dt) == 3:
+                    if val == "100.00%":
+                        print ('Cvc: {}'.format(dt))
+                        nCvc = dt
+                        pCvc = val
             
-    return {Tarjeta{nTarjetanTarjeta,nValnVal,fExpirafExpira,fValfVal}}
+    return {"Tarjeta":{"nTarjeta":nTarjeta,"nVal":nVal,"fExpira":fExpira,"fVal":fVal, "nCvc":nCvc, "pCvc":pCvc, "stringsValues":stringsValues}}
     
     
     return {
-        'statusCode' 200,
-        'body' json.dumps('Hello from Lambda!')
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
     }
